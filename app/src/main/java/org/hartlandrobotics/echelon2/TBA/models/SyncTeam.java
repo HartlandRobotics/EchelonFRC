8a5d44a16cc81feaa0bae3a793dd5236a31f7043
JWC1928 <32470559+JWC1928@users.noreply.github.com>
2022-01-15 14:20:32 -0500

    Deleted irrelevant data points being pulled from blue alliance

--- app/src/main/java/org/hartlandrobotics/echelon2/TBA/models/SyncTeam.java
+++ app/src/main/java/org/hartlandrobotics/echelon2/TBA/models/SyncTeam.java
@@ -18,38 +18,6 @@ public class SyncTeam {
     @JsonProperty("school_name")
     private String schoolName;
 
-    @JsonProperty("city")
-    private String city;
-
-    @JsonProperty("state_prov")
-    private String stateProv;
-
-    @JsonProperty("country")
-    private String country;
-
-    @JsonProperty("address")
-    private String address;
-
-    @JsonProperty("postal_code")
-    private String postalCode;
-
-    @JsonProperty("gmaps_place_id")
-    private String googleMapsPlaceId;
-
-    @JsonProperty("gmaps_url")
-    private String googleMapsUrl;
-
-    @JsonProperty("location_name")
-    private String locationName;
-
-    @JsonProperty("website")
-    private String website;
-
-    @JsonProperty("rookie_year")
-    private int rookieYear;
-
-    @JsonProperty("motto")
-    private String motto;
 
     public String getTeamKey() {
         return teamKey;
@@ -70,49 +38,6 @@ public class SyncTeam {
     public String getSchoolName() {
         return schoolName;
     }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getStateProv() {
-        return stateProv;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public String getGoogleMapsPlaceId() {
-        return googleMapsPlaceId;
-    }
-
-    public String getGoogleMapsUrl() {
-        return googleMapsUrl;
-    }
-
-    public String getLocationName() {
-        return locationName;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public int getRookieYear() {
-        return rookieYear;
-    }
-
-    public String getMotto() {
-        return motto;
-    }
+    
 
 }
