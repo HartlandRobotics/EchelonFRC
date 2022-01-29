23ed7aeb2685c23dfe5cc753fb595b13e155e63b
JWC1928 <32470559+JWC1928@users.noreply.github.com>
2022-01-29 15:21:50 -0500

    started populating the different tabs for TBA pulling. Need to create repos and daos for Event and Matches.

---
+++ app/src/main/java/org/hartlandrobotics/echelon2/TBA/TBAActivity.java
@@ -0,0 +1,40 @@
+package org.hartlandrobotics.echelon2.TBA;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.hartlandrobotics.echelon2.R;
+
+public class TBAActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    TBAPagerAdapter tbaPagerAdapter;
+
+
+    public static void launch(Context context){
+        Intent intent = new Intent(context, TBAActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tba);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        tbaPagerAdapter = new TBAPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(tbaPagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(tbaPagerAdapter.getTabTitle(position))).attach();
+    }
+}
