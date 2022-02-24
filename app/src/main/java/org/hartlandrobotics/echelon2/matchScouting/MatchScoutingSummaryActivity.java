c98b877e0ca5a50cbf9f6a36403d9b9a396180dd
mdemeniuk07 <20014384@hartlandschools.us>
2022-02-23 19:59:26 -0500

    Started match summary screen

---
+++ app/src/main/java/org/hartlandrobotics/echelon2/matchScouting/MatchScoutingSummaryActivity.java
@@ -0,0 +1,14 @@
+package org.hartlandrobotics.echelon2.matchScouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchScoutingSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_summary);
+    }
+}
