68ac278e3e66d73db1e1cda8d2bae34e994b9464
mdemeniuk07 <20014384@hartlandschools.us>
2022-02-26 15:17:25 -0500

    Finished match summary screen

--- app/src/main/java/org/hartlandrobotics/echelon2/matchScouting/MatchScoutingSummaryActivity.java
+++ app/src/main/java/org/hartlandrobotics/echelon2/matchScouting/MatchScoutingSummaryActivity.java
@@ -2,9 +2,25 @@ package org.hartlandrobotics.echelon2.matchScouting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import org.hartlandrobotics.echelon2.R;
+
 public class MatchScoutingSummaryActivity extends AppCompatActivity {
+
+    private static final String MATCH_KEY = "auto_match_key_param";
+    private static final String TEAM_KEY = "auto_team_key_param";
+
+    public static void launch(Context context, String matchKey, String teamKey){
+        Intent intent = new Intent(context, MatchScoutingSummaryActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(MATCH_KEY, matchKey);
+        bundle.putString(TEAM_KEY, teamKey);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
