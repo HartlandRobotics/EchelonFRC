399577816456287119eb5039dc54677c5d3bbe41
Dennis Burton <dennis.burton@gmail.com>
2025-01-06 13:22:18 -0500

    big move to echelon FRC

---
+++ app/src/main/java/org/hartlandrobotics/echelonFRC/models/MatchScoringSummaryModel.java
@@ -0,0 +1,12 @@
+package org.hartlandrobotics.echelonFRC.models;
+
+public class MatchScoringSummaryModel {
+    public String eventKey;
+    public String teamKey;
+    public String matchKey;
+    public String scoringType;
+    public String totalCt;
+    public String totalScore;
+    public String avgCycleTime;
+
+}
