6d7cd7ecbe9b97d7c7a7b7c54e3213f06e3f9832
Elka <56512283+mdemeniuk07@users.noreply.github.com>
2025-03-15 14:05:15 -0400

    Alliance selection, teams and list
    none of  the numbers are in yet, but the teams are there, so is the list

--- app/src/main/java/org/hartlandrobotics/echelonFRC/charts/AllianceSelectionFragment.java
+++ app/src/main/java/org/hartlandrobotics/echelonFRC/charts/AllianceSelectionFragment.java
@@ -1,5 +1,6 @@
 package org.hartlandrobotics.echelonFRC.charts;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,9 +11,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+
+import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.textview.MaterialTextView;
 
 import org.hartlandrobotics.echelonFRC.R;
 
@@ -21,9 +29,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AllianceSelectionFragment extends Fragment {
-    private AutoCompleteTextView teamNumberAutoComplete;
+    private AutoCompleteTextView teamNumber1AutoComplete;
+    private AutoCompleteTextView teamNumber2AutoComplete;
+    private AutoCompleteTextView teamNumber3AutoComplete;
     private List<String> sortedTeamNumbers;
     ChartsActivity.TeamDataViewModel teamData;
+
+    private ListView teamNumberListView;
+    private ListViewItemCheckboxBaseAdapter teamListAdapter;
+
+    private List<TeamListViewModel> allTeamNumbers;
 
     public AllianceSelectionFragment() {
         // Required empty public constructor
@@ -39,8 +54,8 @@ public class AllianceSelectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_alliance_selection, container, false);
-        teamNumberAutoComplete = view.findViewById(R.id.team1SelectionAutoComplete);
-        teamNumberAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        teamNumber1AutoComplete = view.findViewById(R.id.team1SelectionAutoComplete);
+        teamNumber1AutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
                 Log.i("AllianceSelectionFragment", "item clicked");
@@ -48,6 +63,27 @@ public class AllianceSelectionFragment extends Fragment {
                 teamData = ((ChartsActivity) getActivity()).getTeamData(teamNumber);
             }
         });
+
+        teamNumber2AutoComplete = view.findViewById(R.id.team2SelectionAutoComplete);
+        teamNumber2AutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Log.i("AllianceSelectionFragment", "item clicked");
+                String teamNumber = sortedTeamNumbers.get(position);
+                teamData = ((ChartsActivity) getActivity()).getTeamData(teamNumber);
+            }
+        });
+
+        teamNumber3AutoComplete = view.findViewById(R.id.team3SelectionAutoComplete);
+        teamNumber3AutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Log.i("AllianceSelectionFragment", "item clicked");
+                String teamNumber = sortedTeamNumbers.get(position);
+                teamData = ((ChartsActivity) getActivity()).getTeamData(teamNumber);
+            }
+        });
+
         return view;
     }
 
@@ -71,7 +107,64 @@ public class AllianceSelectionFragment extends Fragment {
             return;
         }
         Log.i("AllianceSelectionFragment", "setupDropDown with" + sortedTeamNumbers.size() + "teams");
-        ArrayAdapter teamNumberAdapter = new ArrayAdapter(getContext(), R.layout.dropdown_item, sortedTeamNumbers);
-        teamNumberAutoComplete.setAdapter(teamNumberAdapter);
+        ArrayAdapter teamNumber1Adapter = new ArrayAdapter(getContext(), R.layout.dropdown_item, sortedTeamNumbers);
+        teamNumber1AutoComplete.setAdapter(teamNumber1Adapter);
+        ArrayAdapter teamNumber2Adapter = new ArrayAdapter(getContext(), R.layout.dropdown_item, sortedTeamNumbers);
+        teamNumber2AutoComplete.setAdapter(teamNumber2Adapter);
+        ArrayAdapter teamNumber3Adapter = new ArrayAdapter(getContext(), R.layout.dropdown_item, sortedTeamNumbers);
+        teamNumber3AutoComplete.setAdapter(teamNumber3Adapter);
+    }
+
+    public class ListViewItemCheckboxBaseAdapter extends BaseAdapter {
+        Context context;
+        List<TeamListViewModel> teamViewModels;
+
+        public ListViewItemCheckboxBaseAdapter(Context context) {
+            this.context = context;
+        }
+
+        public void setTeams(List<TeamListViewModel> teamViewModels){
+            this.teamViewModels = teamViewModels;
+        }
+
+        @Override
+        public int getCount() {
+            if( this.teamViewModels == null ) return 0;
+            return this.teamViewModels.size();
+        }
+
+        @Override
+        public TeamListViewModel getItem(int index) {
+            return this.teamViewModels.get(index);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position,View view,ViewGroup parent) {
+            LayoutInflater inflater=getActivity().getLayoutInflater();
+            View rowView=inflater.inflate(R.layout.list_item_alliance_selection, null,true);
+
+            MaterialTextView teamNumber = rowView.findViewById(R.id.team_number);
+            MaterialCheckBox teamSelectedCheckBox = rowView.findViewById(R.id.team_visible);
+
+            TeamListViewModel teamListViewModel = teamViewModels.get(position);
+            teamNumber.setText(teamListViewModel.getTeamNumber());
+            teamSelectedCheckBox.setChecked(teamListViewModel.getIsSelected());
+            teamSelectedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    ViewParent layoutViewParent = buttonView.getParent();
+                    ListView listView =  (ListView) layoutViewParent.getParent();
+                    int position = listView.getPositionForView(buttonView);
+                    teamViewModels.get(position).setIsSelected(isChecked);
+
+                    //setVisibleTeams();
+                    }
+            });
+            return rowView;
+        };
     }
 }
