26aa231db7189a245916aab91444c9642eff8920
Elka <56512283+mdemeniuk07@users.noreply.github.com>
2026-01-26 21:04:55 -0500

    Teleop screen layout

--- app/src/main/java/org/hartlandrobotics/echelonFRC/pitScouting/PitScoutTeleOpFragment.java
+++ app/src/main/java/org/hartlandrobotics/echelonFRC/pitScouting/PitScoutTeleOpFragment.java
@@ -20,9 +20,9 @@ public class PitScoutTeleOpFragment extends Fragment {
     public static final String TAG = "PitScoutTeleOpFragment";
 
 
-    RadioGroup pickOffGround;
-    TextInputLayout scoringMethodLayout;
-    RadioGroup defenseGroup;
+//    RadioGroup pickOffGround;
+//    TextInputLayout scoringMethodLayout;
+//    RadioGroup defenseGroup;
     PitScout data;
 
     boolean areControlsSetup = false;
@@ -46,7 +46,7 @@ public class PitScoutTeleOpFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pitscout_tele_op, container, false);
 
-        setupControls(view);
+        //setupControls(view);
 
         return view;
     }
@@ -55,49 +55,49 @@ public class PitScoutTeleOpFragment extends Fragment {
     public void onResume(){
         super.onResume();
         Log.i(TAG, "on Resume");
-        populateControlsFromData();
+        //populateControlsFromData();
     }
 
     @Override
     public void onPause(){
         super.onPause();
         Log.i(TAG, "on Pause");
-        populateDataFromControls();
+        //populateDataFromControls();
     }
 
     private void setupControls(View view) {
             if (data == null) {
                 return;
             }
-        pickOffGround = view.findViewById(R.id.pickOffGround);
-        pickOffGround.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                boolean offGroundYes = checkedId == R.id.offGroundYes;
-                data.setOffGroundYes(offGroundYes);
-            }
-        });
-
-        scoringMethodLayout = view.findViewById(R.id.scoringMethod);
-        String scoringMethod = StringUtils.defaultIfBlank(data.getScoringMethod(), StringUtils.EMPTY);
-        scoringMethodLayout.getEditText().setText(scoringMethod);
-
-        defenseGroup = view.findViewById(R.id.robotDefense);
-
-        areControlsSetup = true;
+//        pickOffGround = view.findViewById(R.id.rolePrefrence);
+//        pickOffGround.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                boolean offGroundYes = checkedId == R.id.offGroundYes;
+//                data.setOffGroundYes(offGroundYes);
+//            }
+//        });
+//
+//        scoringMethodLayout = view.findViewById(R.id.scoringMethod);
+//        String scoringMethod = StringUtils.defaultIfBlank(data.getScoringMethod(), StringUtils.EMPTY);
+//        scoringMethodLayout.getEditText().setText(scoringMethod);
+//
+//        defenseGroup = view.findViewById(R.id.robotDefense);
+//
+//        areControlsSetup = true;
     }
 
     public void populateDataFromControls() {
         Log.i(TAG, "populate data from controls");
 
-        boolean offGroundYes = pickOffGround.getCheckedRadioButtonId() == R.id.offGroundYes;
-        data.setOffGroundYes(offGroundYes);
-
-        boolean canPlayDefense = defenseGroup.getCheckedRadioButtonId() == R.id.robotDefenseYes;
-        data.setCanPlayDefense(canPlayDefense);
-
-        String scoringMethod = scoringMethodLayout.getEditText().getText().toString();
-        data.setScoringMethod(scoringMethod);
+//        boolean offGroundYes = pickOffGround.getCheckedRadioButtonId() == R.id.offGroundYes;
+//        data.setOffGroundYes(offGroundYes);
+//
+//        boolean canPlayDefense = defenseGroup.getCheckedRadioButtonId() == R.id.robotDefenseYes;
+//        data.setCanPlayDefense(canPlayDefense);
+//
+//        String scoringMethod = scoringMethodLayout.getEditText().getText().toString();
+//        data.setScoringMethod(scoringMethod);
     }
 
     private void populateControlsFromData() {
@@ -107,17 +107,17 @@ public class PitScoutTeleOpFragment extends Fragment {
         }
 
         // check that controls have been established
-        if(pickOffGround == null) return;
-
-
-        Log.i(TAG, "populate controls from data");
-        boolean offGroundYes = data.getPickOffGround();
-        int offGroundYesSelection = offGroundYes ? R.id.offGroundYes : R.id.offGroundNo;
-        pickOffGround.check(offGroundYesSelection);
-
-
-        boolean canPlayDefense = data.getCanPlayDefense();
-        int canPlayDefenseSelection = canPlayDefense ? R.id.robotDefenseYes : R.id.robotDefenseNo;
-        defenseGroup.check(canPlayDefenseSelection);
+//        if(pickOffGround == null) return;
+//
+//
+//        Log.i(TAG, "populate controls from data");
+//        boolean offGroundYes = data.getPickOffGround();
+//        int offGroundYesSelection = offGroundYes ? R.id.offGroundYes : R.id.offGroundNo;
+//        pickOffGround.check(offGroundYesSelection);
+//
+//
+//        boolean canPlayDefense = data.getCanPlayDefense();
+//        int canPlayDefenseSelection = canPlayDefense ? R.id.robotDefenseYes : R.id.robotDefenseNo;
+//        defenseGroup.check(canPlayDefenseSelection);
     }
 }
