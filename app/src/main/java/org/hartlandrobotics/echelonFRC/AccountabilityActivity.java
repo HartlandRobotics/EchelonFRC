2f0145a3b10029a507298040b95479ca6effe5fa
Dennis Burton <dennis.burton@gmail.com>
2026-02-11 22:12:05 -0500

    changes to activity

--- app/src/main/java/org/hartlandrobotics/echelonFRC/AccountabilityActivity.java
+++ app/src/main/java/org/hartlandrobotics/echelonFRC/AccountabilityActivity.java
@@ -10,22 +10,28 @@ import androidx.annotation.Nullable;
 import org.hartlandrobotics.echelonFRC.database.currentGame.CurrentGamePoints;
 import org.hartlandrobotics.echelonFRC.database.entities.Match;
 import org.hartlandrobotics.echelonFRC.database.entities.MatchResult;
+import org.hartlandrobotics.echelonFRC.database.repositories.EventRepo;
 import org.hartlandrobotics.echelonFRC.database.repositories.MatchRepo;
 import org.hartlandrobotics.echelonFRC.database.repositories.MatchResultRepo;
+import org.hartlandrobotics.echelonFRC.status.BlueAllianceStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class AccountabilityActivity extends EchelonActivity {
 
     String TAG = "AccountabilityActivity";
+    EventRepo eventRepo;
     MatchRepo matchRepo;
     MatchResultRepo matchResultRepo;
 
-    public static void launch(Context context){
-        Intent intent = new Intent( context, AccountabilityActivity.class );
+    public static void launch(Context context) {
+        Intent intent = new Intent(context, AccountabilityActivity.class);
         context.startActivity(intent);
     }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,46 +39,46 @@ public class AccountabilityActivity extends EchelonActivity {
 
         setupToolbar("Accuracy");
 
-        matchRepo= new MatchRepo(getApplication());
-        matchResultRepo= new MatchResultRepo(getApplication());
-        matchRepo.getMatches().observe( this, lm -> {
+        BlueAllianceStatus status = new BlueAllianceStatus(getApplicationContext());
+        String currentEvent = status.getEventKey();
+
+        eventRepo = new EventRepo(getApplication());
+        matchRepo = new MatchRepo(getApplication());
+        matchResultRepo = new MatchResultRepo(getApplication());
+
+        eventRepo.getMatchesForEvent(currentEvent).observe(this, e -> {
+            matchResultRepo.getMatchResultsByEvent(currentEvent).observe(this, matchResultList -> {
+                for (Match m : e.matches) {
+                    String currentMatchKey = m.getMatchKey();
 
 
-            for (Match match:lm){
-                List<String> allianceTeams = new ArrayList<String>();
-                match.getBlueScore();
+                    int BlueScore = m.getBlueScore();
+                    int RedScore = m.getRedScore();
 
-                String matchKey = match.getMatchKey();
+                    int studentRedScore = 0;
+                    int studentBlueScore = 0;
+                    List<MatchResult> matchResultByMatch = matchResultList.stream()
+                            .filter(mr -> mr.getMatchKey().equals(currentMatchKey))
+                            .collect(Collectors.toList());
 
-                int blueBlueAllianceScore = match.getBlueScore();
-                allianceTeams.add(match.getBlue1TeamKey());
-                allianceTeams.add(match.getBlue2TeamKey());
-                allianceTeams.add(match.getBlue3TeamKey());
-
-                List<String> redAllianceTeams = new ArrayList<String>();
-                match.getRedScore();
-
-                int redBlueAllianceScore = match.getRedScore();
-                allianceTeams.add(match.getRed1TeamKey());
-                allianceTeams.add(match.getRed2TeamKey());
-                allianceTeams.add(match.getRed3TeamKey());
-
-                for(String teamKey : allianceTeams) {
-                    matchResultRepo.getMatchResultByMatchTeam(matchKey, teamKey).observe(this, mr -> {
-                        if( mr != null) {
-                            Log.i(TAG, mr.getAlliance());
-                            CurrentGamePoints pts =  new CurrentGamePoints(mr);
+                    for (MatchResult mr : matchResultByMatch) {
+                        String alliance = mr.getAlliance();
+                        CurrentGamePoints cg = new CurrentGamePoints(mr);
+                        if (alliance.equals("red")) {
+                            studentRedScore += cg.getAutoPoints() + cg.getTeleOpPoints() + cg.getEndPoints();
                         }
-                    });
+                        if (alliance.equals("blue")) {
+                            studentBlueScore += cg.getAutoPoints() + cg.getTeleOpPoints() + cg.getEndPoints();
+                        }
+                    }
+
+
+
                 }
-                int i;
-                i=10;
-            }
+            });
 
-
-
-            // red alliance
         });
+
 
     }
 
